529b1ad83941a6f45bda7c164840a35c88294253
jiuqi <eric2625558982@gmail.com>
2021-07-11 15:16:36 +0200

    create db connection

--- hungary-realestate-api/main.py
+++ hungary-realestate-api/main.py
@@ -5,5 +5,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=8000, reload=True, debug=True, workers=1)
